a927e77c3ed98ffdf75bd37a1c0ceb8326b1a16c
Dize dev <axiozweb2022@gmail.com>
2024-05-14 00:41:43 -0500

    se muesta el estilo y la infoamrcion dependiendo de la notificacion

--- js/app.js
+++ js/app.js
@@ -2,17 +2,38 @@
 const buttons = document.querySelectorAll('.buttons .btn'),
     notification = document.querySelector('.notifications');
 
+const toastDetails = {
+    timer: 5000,
+    success: {
+        icon: 'fa-circle-check',
+        text: 'Success: This is a success toast.',
+    },
+    error: {
+        icon: 'fa-circle-xmark',
+        text: 'Error: This is an error toast.',
+    },
+    warning: {
+        icon: 'fa-triangle-exclamation',
+        text: 'Warning: This is a warning toast.',
+    },
+    info: {
+        icon: 'fa-circle-info',
+        text: 'Info: This is an information toast.',
+    }
+}
+
 const createToast = (id) => {
+    const { icon, text } = toastDetails[id];
     const toast = document.createElement('li');
     toast.className = `toast ${id}`;
 
     toast.innerHTML = `
         <div class="column">
-          <i class="fa-solid "></i>
-          <span></span>
+           <i class="fa-solid ${icon}"></i>
+            <span>${text}</span>
         </div>
         <i class="fa-solid fa-xmark" onclick="removeToast(this.parentElement)"></i>
-    `;
+     `;
 
     notification.appendChild(toast);
 };
